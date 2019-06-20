015318fad26aeb8d67da5565f89cfe888f5c6d91
Ankita Pabbi <erankitapabbi@gmail.com>
2019-06-20 19:31:14 -0400

    [Ankita Pabbi ] Created 4 Objects of Customer

--- C0751145_MID_TERM_MAD3004/C0751145_MID_TERM_MAD3004/main.swift
+++ C0751145_MID_TERM_MAD3004/C0751145_MID_TERM_MAD3004/main.swift
@@ -8,5 +8,11 @@
 
 import Foundation
 
-// creating Objects of Customer 
+// creating Objects of Customer
+var customer_1: Customer = Customer.init(c_id: 1, f_name: "Vishal", l_name: "Pabbi", e_id: "vishal@gmail.com", t_amt: 1000)
+var customer_2: Customer = Customer.init(c_id: 2, f_name: "Diksha", l_name: "Pabbi", e_id: "diksha@gmail.com", t_amt: 500)
+var customer_3: Customer = Customer.init(c_id: 3, f_name: "Netra", l_name: "Sharma", e_id: "ns@gmail.com", t_amt: 600)
+var customer_4: Customer = Customer.init(c_id: 4, f_name: "Nisha", l_name: "Verma", e_id: "nverma@gmail.com", t_amt: 800)
+
+
 
