d118b59172146efff8e343d030728aa5918a40be
Ankita Pabbi <erankitapabbi@gmail.com>
2019-06-20 18:41:55 -0400

    [Ankita Pabbi] Created a basic structure of Projects including all the classes and protocol

---
+++ C0751145_MID_TERM_MAD3004/C0751145_MID_TERM_MAD3004/Bill.swift
@@ -0,0 +1,10 @@
+//
+//  Bill.swift
+//  C0751145_MID_TERM_MAD3004
+//
+//  Created by Ankita Pabbi on 2019-06-20.
+//  Copyright © 2019 Ankita Pabbi. All rights reserved.
+//
+
+import Foundation
+
